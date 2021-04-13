8e3a405067e9ebe82ecc69570dbf75ba25d91f9b
Marcelo <marcelo.cansilva@gmail.com>
2021-04-13 00:08:43 -0300

    Complete css and add text

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'games#new'
   get 'new', to: 'games#new'
-  post 'score', to: 'games#score'
+  post 'score', to: 'games#score', as: :games#new
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
